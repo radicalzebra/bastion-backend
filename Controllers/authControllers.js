99cb33929634e69b7a4c635c0fcca2f345dd70fd
Arshad Faiyaz <ourself786@gmail.com>
2023-11-02 16:28:06 +0530

    Update authControllers.js

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -75,9 +75,9 @@ exports.protect = catchAsync(async (req,res,next) => {
 
    //Getting token and heck if it's there
    let token;
-   if(req.cookies.jwt) {
-      token = req.cookies.jwt
-   };
+    if(req.headers.authorization && req.headers.authorization.startsWith(`Bearer`)) {
+        token = req.headers.authorization.split(" ")[1];
+    } else if (req.cookies.jwt)  token = req.cookies.jwt;
 
    if(!token) next(new MyError("Please login to get access!",401));
 
