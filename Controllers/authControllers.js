c5f0d5213e304b64764227e66fb3897f1edefea3
Arshad Faiyaz <ourself786@gmail.com>
2023-11-02 16:45:40 +0530

    Update authControllers.js

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -19,7 +19,7 @@ const signToken = (id) => {
 const createSendToken = (user,statusCode,res) => {
    const token = signToken(user._id);
 
-   const cookeOptions = {
+   const cookieOptions = {
       expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
       secure: false,
       httpOnly: true
