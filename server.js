c6f74dfcbdd382c6b44e1c1326e0ff5850daea37
Arshad Faiyaz <ourself786@gmail.com>
2023-11-02 16:52:16 +0530

    fixed some mistakes

--- server.js
+++ server.js
@@ -3,13 +3,23 @@ dotenvv.config({ path:"./config.env" })
 const mongoose = require("mongoose")
 const app = require("./app")
 
+// mongoose.connect(process.env.DATABASE).then(con => {
+//    console.log("DB connection successfull...")
+// })
+
+// const server = app.listen(process.env.PORT,()=>{
+//    console.log(`Server is listening...`)
+// })
+
+let server
+
 mongoose.connect(process.env.DATABASE).then(con => {
+   server = app.listen(process.env.PORT,()=>{
+   console.log(`Server is listening...`)
+   })
    console.log("DB connection successfull...")
 })
 
-const server = app.listen(process.env.PORT,()=>{
-   console.log(`Server is listening...`)
-})
 
 
 process.on("unhandledRejection" , (err) => {
