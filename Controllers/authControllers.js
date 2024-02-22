009fd222bd22ac06bfb24e6f54cbd935001fc9b2
Arshad Faiyaz <ourself786@gmail.com>
2024-02-23 02:10:34 +0530

    Update authControllers.js

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -21,11 +21,12 @@ const createSendToken = (user,statusCode,res) => {
 
    const cookieOptions = {
       expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-      secure: false,
+      // secure: false,
+      secure: true,
       httpOnly: true
    }
 
-   if(process.env.NODE_ENV === "production") cookieOptions.secure = true
+   // if(process.env.NODE_ENV === "production") cookieOptions.secure = true
 
 
 
