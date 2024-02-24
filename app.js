21058354eda6e96d29f8a28530825c05fff1d96e
Arshad Faiyaz <ourself786@gmail.com>
2024-02-24 15:08:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,15 @@ app.use(cors({
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  
+  const allowedOrigins = ['http://localhost:5173', 'http://example.com'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  
+  // res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
