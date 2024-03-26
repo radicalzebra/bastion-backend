286e42b0c3b1b671a6abcf745f22ada40f373974
Arshad Faiyaz <ourself786@gmail.com>
2024-03-27 04:49:06 +0530

    Update reviewRoutes.js

--- Routes/reviewRoutes.js
+++ Routes/reviewRoutes.js
@@ -6,13 +6,13 @@ const authControllers = require("../Controllers/authControllers")
 
 const router = express.Router({mergeParams:true});
 
-console.log("001")
+
 
 router.route("/")
       .get(reviewControllers.getAllReviews)
       .post(authControllers.protect,authControllers.restrictTo("user","seller"),reviewControllers.createReview)
 
-console.log("end")
+
 
 router.route("/:reviewId")
       .patch(authControllers.protect,reviewControllers.checkReviewer,reviewControllers.updateReview)
