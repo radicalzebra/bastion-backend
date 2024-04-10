e51ea618ca810a7d7115e10b8698734f867f5b95
Arshad Faiyaz <ourself786@gmail.com>
2024-04-10 15:50:33 +0530

    Update purchaseRoutes.js

--- Routes/purchaseRoutes.js
+++ Routes/purchaseRoutes.js
@@ -6,6 +6,6 @@ const authControllers = require("../Controllers/authControllers")
 const router = express.Router();
 
 router.post("/checkout/:productId",authControllers.protect,purchaseControllers.preventOwnPurchase,purchaseControllers.getCheckoutSession)
-router.get("/", authControllers.protect,authControllers.restrictTo("admin"),purchaseControllers.getAllPurchases)
+router.get("/", authControllers.protect,authControllers.restrictTo("admin", "seller"),purchaseControllers.getAllPurchases)
 
 module.exports = router
