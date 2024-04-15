910facdd3f4b1e44f8ff404c45106064bb3579ec
Arshad Faiyaz <ourself786@gmail.com>
2024-04-15 15:27:10 +0530

    Update purchaseModal.js

--- Models/purchaseModal.js
+++ Models/purchaseModal.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+const Product = require("./")
 const purchaseSchema = new mongoose.Schema({
 
    product:{
@@ -43,6 +43,36 @@ purchaseSchema.pre("find", function(next) {
 
    next()
 })
+
+purchaseSchema.statics.calcSold = async function(productId) {
+ 
+   const stats = await this.aggregate([
+      {
+         $match : {product : productId}
+      }, 
+
+      {
+         $group : {
+            _id:"$product",
+            sold: {$sum : 1},
+         }
+      }
+   ])
+
+   await Product.findByIdAndUpdate(productId,{
+      sold:stats[0].sold
+   },{
+      new:true,
+      runValidators:true
+   })
+}
+
+
+purchaseSchema.post("save",function(doc,next){
+   this.constructor.calcSold(this.product)
+   next()
+})
+
 
 const Purchase = mongoose.model("Purchases",purchaseSchema)
 
