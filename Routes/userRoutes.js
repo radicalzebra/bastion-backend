107d5f5e31a001e24fad2e9df485ae62e771ccdb
Arshad Faiyaz <ourself786@gmail.com>
2024-05-02 11:54:25 +0530

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -13,11 +13,12 @@ router.post("/login",authController.login);
 router.post("/forgot-password",authController.forgotPassword);
 router.post("/reset-password/:resetToken",authController.protect,authController.resetPassword);
 router.patch("/update-password", authController.protect,authController.updatePassword);
-router.patch("/updateMe",authController.protect,userControllers.uploadUserPhoto,userControllers.resizeUserPhoto,userControllers.updateMe);
+// router.patch("/updateMe",authController.protect,userControllers.uploadUserPhoto,userControllers.resizeUserPhoto,userControllers.updateMe);
+router.patch("/updateMe",authController.protect,userControllers.updateMe);
 router.delete("/deleteMe",authController.protect,userControllers.deleteMe);
 router.put("/updateCart",authController.protect,userControllers.updateCart);
 router.get("/me",authController.protect,userControllers.getMe,userControllers.getUser)
-// router.get("/images/:img",userControllers.getImage)
+
 
 
 
