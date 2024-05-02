9c6725b2a478b1eca50a7fd693ffe8f2ad7c1079
Arshad Faiyaz <ourself786@gmail.com>
2024-05-02 11:43:47 +0530

    Update productRoutes.js

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -6,16 +6,19 @@ const reviewRouter = require("../Routes/reviewRoutes")
 
 const router = express.Router();
 
+// the two post and patch route handlers are commented because there was some problem with the img processing and was unable to diagnose the problem
 
 router.route("/")
 .get(productControllers.getAllProducts)
-.post(authControllers.protect,authControllers.restrictTo("seller"),productControllers.uploadProductImages,productControllers.resizeProductImages,productControllers.createProduct)
+// .post(authControllers.protect,authControllers.restrictTo("seller"),productControllers.uploadProductImages,productControllers.resizeProductImages,productControllers.createProduct)
+.post(authControllers.protect,authControllers.restrictTo("seller"),productControllers.createProduct)
 
 router.use("/:productId/reviews", reviewRouter)
 
 router.route("/:id")
 .get(productControllers.getProduct)
-.patch(authControllers.protect,authControllers.restrictTo("seller"),productControllers.uploadProductImages,productControllers.resizeProductImages,productControllers.updateProduct)
+// .patch(authControllers.protect,authControllers.restrictTo("seller"),productControllers.uploadProductImages,productControllers.resizeProductImages,productControllers.updateProduct)
+.patch(authControllers.protect,authControllers.restrictTo("seller"),productControllers.updateProduct)
 .delete(authControllers.protect,productControllers.deleteProduct)
 
 
